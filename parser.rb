bf9a6e0c6c09edda9a14ccb5fa466b242b29a46a
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2014-05-27 08:42:19 +0200

    parser class draft

---
+++ parser.rb
@@ -0,0 +1,10 @@
+class Parser
+  attr_accessor :document
+
+  def initialize(document)
+    @document = document
+  end
+
+  def extract_data
+  end
+end
--- scraper.rb
+++ scraper.rb
@@ -6,15 +6,22 @@ require 'active_support'
 require 'active_support/core_ext/object'
 require 'colored'
 
+require_relative 'parser'
+
 def page_url(page)
   base_url = 'http://nimoz.pl/pl/bazy-danych/wykaz-muzeow-w-polsce/baza-muzeow-w-polsce'
 
   "#{base_url}?page=#{page}"
 end
 
+
 from_page = 1
 to_page = 18
 
+museums = []
+
 (from_page .. to_page).each do |page|
   doc = Nokogiri::HTML(open(page_url(page)))
+  museums << Parser.new(doc).extract_data
 end
+
